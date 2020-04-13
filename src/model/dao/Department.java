582a1c8a20a1dd4e1bc0b8c29994873adb6bcd60
Wallace Vinicius <wallacevncs@gmail.com>
2020-04-13 11:03:37 -0300

    findByDepartment implementation

--- src/model/dao/Department.java
+++ src/model/dao/Department.java
@@ -2,14 +2,11 @@ package model.dao;
 
 import java.util.List;
 
-import model.entities.Department;
-
-public interface Departament {
+public interface Department {
 	
 	void insert(Department obj);
 	void update(Department obj);
 	void deleteById(Integer id);
 	Department findById(Integer id);
 	List<Department> findAll();
-
 }
